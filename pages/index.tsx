67c72ace902e5c886072af4e46083259ff3bc85b
Ivan Ebos <ivanebos68@gmail.com>
2023-03-31 11:47:31 -0400

    debug

--- pages/index.tsx
+++ pages/index.tsx
@@ -87,7 +87,7 @@ const Home: NextPage = () => {
 
             <li className="hover:scale-110 transition-all">
               <a
-                href="../public/resume.pdf"
+                href="public/resume.pdf"
                 download
                 className=" border-2 border-blue-500 text-blue-500 px-4 py-2 rounded-md ml-8 hover:bg-blue-500 hover:text-white transition-all"
               >
